ebcb3834bae26f1fd4922265e77dc55e7f284947
foz1284 <foz1284@bc42c6d7-46c2-524d-0085-0bbed8e8ee69>
2012-01-31 22:00:25 +0000

    tidyed up usings

--- trunk/src/pl/polidea/treeview/AbstractTreeViewAdapter.java
+++ trunk/src/pl/polidea/treeview/AbstractTreeViewAdapter.java
@@ -10,7 +10,6 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
-import android.widget.FrameLayout.LayoutParams;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 import android.widget.LinearLayout;
