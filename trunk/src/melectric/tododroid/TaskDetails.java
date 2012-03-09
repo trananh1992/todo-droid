f2ecd94bba79b26ea6ab1a09058cc3ba88dc4198
Anton.Burkun@gmail.com <Anton.Burkun@gmail.com@bc42c6d7-46c2-524d-0085-0bbed8e8ee69>
2012-03-09 21:56:35 +0000

    fixing taskdetails view issue

--- trunk/src/melectric/tododroid/TaskDetails.java
+++ trunk/src/melectric/tododroid/TaskDetails.java
@@ -10,13 +10,21 @@ import android.os.Bundle;
 import android.widget.EditText;
 
 public class TaskDetails extends Activity {
+	
+    private Long taskId;
+
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.taskdetails);
          
-        int taskId = getIntent().getIntExtra("TaskId", 0);
+        //int taskId = getIntent().getIntExtra("TaskId", 0);
+		Bundle extras = getIntent().getExtras();
+		taskId = extras != null ? extras.getLong("TaskId")
+								: null;
+
 
         SQLiteDatabase myDB = this.openOrCreateDatabase("TaskListDatabase", MODE_PRIVATE, null);
         Cursor c = myDB.rawQuery("SELECT * FROM t_Tasks WHERE Id = " + taskId, null);
@@ -26,7 +34,9 @@ public class TaskDetails extends Activity {
             do {
             	Task task = new Task(c, myDB);
             	EditText title =(EditText) findViewById(R.id.TitleTextBox);
+            	EditText id =(EditText) findViewById(R.id.IDTextBox);
             	title.setText(task.Title);
+            	id.setText(Long.toString(taskId));
                
             } while (c.moveToNext());
         }  
